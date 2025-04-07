b67e0e6b79bb20e3378a0fc80a4cc0ac9e15bc22
Zeynep Ecem Colak <z.colak@tu-braunschweig.de>
2025-04-07 20:51:37 +0200

    update - Rest API endpoint

--- api.py
+++ api.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Smart Support Bot API is working!"}
+
+@app.get("/faq")
+def get_faq():
+    with open("faq_data.txt", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    return {"faq": [line.strip() for line in lines if line.strip()]}
+
